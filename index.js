9bc19c7cdde96bc2d07a996d5b5accdb7996c317
Denno31 <denniskyn80@gmail.com>
2022-10-13 19:10:31 +0300

    added json parser

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require("express")
 const app = express()
 const PORT = process.env.PORT || 5000;
 const nodemailer = require("nodemailer")
+const bodyParser = require('body-parser')
 
 app.use(express.json())
+app.use(bodyParser.urlencoded({extended:false}))
 app.get("/",(req,res)=>{
     res.send('hello world')
 })
